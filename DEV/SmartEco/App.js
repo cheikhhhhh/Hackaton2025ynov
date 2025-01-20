fd0a41108938395b1d2813ba72a513957bda5f73
cheikhhhhh <cheikhchouiref@gmail.com>
2025-01-20 14:20:44 +0100

    test

--- DEV/SmartEco/App.js
+++ DEV/SmartEco/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ca marche bien </Text>
+      <Text>Ca marche bien !</Text>
       <StatusBar style="auto" />
     </View>
   );
