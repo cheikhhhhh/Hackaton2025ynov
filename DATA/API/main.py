a0b9e5d56b648c29f942a20a29369e19e9f40d2a
MaxGlt <mgaul470@gmail.com>
2025-01-21 21:09:02 +0100

    feat: Use App Runner environment variables instead of .env file

--- DATA/API/main.py
+++ DATA/API/main.py
@@ -1,4 +1,26 @@
+import os
 from flask import Flask
+from Controllers.Controller_Model import prediction_blueprint
+from Controllers.Controller_Database import database_blueprint
+
+bucket_name = os.getenv('BUCKET_NAME')
+file_key = os.getenv('FILE_KEY')
+
+if not bucket_name or not file_key:
+    raise ValueError("Les variables d'environnement BUCKET_NAME et FILE_KEY doivent être définies")
+
+app = Flask(__name__)
+
+app.config['BUCKET_NAME'] = bucket_name
+app.config['FILE_KEY'] = file_key
+
+app.register_blueprint(prediction_blueprint)
+app.register_blueprint(database_blueprint)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+""" from flask import Flask
 from Controllers.Controller_Model import prediction_blueprint
 from Controllers.Controller_Database import database_blueprint
 
@@ -8,4 +30,4 @@ app.register_blueprint(prediction_blueprint)
 app.register_blueprint(database_blueprint)
 
 if __name__ == '__main__':  
-   app.run(debug=True)  
+   app.run(debug=True)   """
