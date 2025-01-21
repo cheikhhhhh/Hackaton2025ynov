ffd883c459134c0aa7248077e4e30c1a75ac333b
MaxGlt <mgaul470@gmail.com>
2025-01-21 23:57:33 +0100

    updating user model

--- DATA/API/Models/User.py
+++ DATA/API/Models/User.py
@@ -9,7 +9,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    carbon_emission = db.Column(db.Float, nullable=True)
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, carbon_emission=None):
         self.username = username
         self.password = generate_password_hash(password)
+        self.carbon_emission = carbon_emission
